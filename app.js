d3fea58f712bd8026eaddd8b1c06d7dcd9675e89
dhivyamadhv <dhivyamadhavan@hclguvi.in>
2026-06-10 11:43:40 +0530

    change yml file,app js and  app python file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const PASSWORD = "admin123";
+const PASSWORD = "admin123";
 app.get("/", (req, res) => {
     const userInput = req.query.cmd;
 
